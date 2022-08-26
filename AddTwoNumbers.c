0f65593d10b523afad79cee512663f87820b2acb
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-08-26 11:59:22 +0530

    Add Two Numbers

---
+++ AddTwoNumbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main() {
+	// Read the number of test cases.
+	int T;
+	scanf("%d", &T);
+	while (T) {
+		// Read the input a, b
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		// Compute the ans.
+		int ans = a + b;
+		printf("%d\n", ans);
+		T --;
+	}
+
+	return 0;
+}
